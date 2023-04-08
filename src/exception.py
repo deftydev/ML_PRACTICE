ea903053d4966bc051ad2b251ce4d81a109e4bc6
deftydev <devanshgupta79212346@gmail.com>
2023-04-08 23:34:05 +0530

    looging and custom exception handling

---
+++ src/exception.py
@@ -0,0 +1,35 @@
+import sys
+import os
+
+from src.logger import logging
+
+def get_error_detail(error_messages,error_detail:sys):
+    _,_,exc_tb= error_detail.exc_info()
+    file_name= exc_tb.tb_frame.f_code.co_filename
+
+    error_message= "Error occured in file named {0}, at line number {1} - error {3}".format(file_name, exc_tb.tb_lineno,error_messages)
+
+    return error_message
+
+
+
+class CustomException(Exception):
+
+    def __init__(error_message, error_detail:sys):
+        super.__init__(error_message)
+        self.error_message= get_error_detail(error_message,error_detail)
+
+
+    def __str__(self) -> str:
+        return self.error_mesage
+    
+
+if __name__=="__main__":
+
+    logging.info('Logging has started')
+
+    try:
+        a=3/0
+    except Exception as e:
+        logging.info('Some error occured')
+        raise CustomException(e,sys)
